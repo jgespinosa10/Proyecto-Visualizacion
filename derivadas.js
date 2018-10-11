3e4bdeec9290c23de289bc52f489b2ab0821f635
jgespinosa10 <jgespinosa@uc.cl>
2018-10-11 14:06:16 -0300

    derivadas con datos

--- derivadas.js
+++ derivadas.js
@@ -1,49 +1,49 @@
-function vecinos(punto, dataset, distancia)
+function vecinos(punto, dataset, distancia, x, y)
 {
     let neighbours = [];
     for(let i = 0; i < dataset.length; i++)
     {
-        let r = ((dataset[i].y - punto.y)**2+(dataset[i].x - punto.x)**2)**(1);
+        let r = ((dataset[i][y] - punto[y])**2+(dataset[i][x] - punto[x])**2)**(1);
         if(r<=distancia) neighbours.push(dataset[i]);
     }
     return neighbours;
 }
 
 // recibe los vecinos y calcula la derivada
-function primera_derivada(punto, neighbours)
+function primera_derivada(punto, neighbours, x, y)
 {
     let derivada = 0;
     let division = 0;
     for(let i = 0; i < neighbours.length; i++)
     {
-        let deltax = neighbours[i].x - punto.x;
-        derivada += (neighbours[i].y - punto.y)*(deltax);
+        let deltax = neighbours[i][x] - punto[x];
+        derivada += (neighbours[i][y] - punto[y])*(deltax);
         division += (deltax)**2;
     }
     return derivada/division;
 }
 
-function segunda_derivada(punto, dataset, distancia)
+function segunda_derivada(punto, dataset, distancia, x, y)
 {
     let derivada = 0;
     let division = 0;
-    let neighbours = vecinos(punto, dataset, distancia)
-    let primera = primera_derivada(punto, neighbours);
+    let neighbours = vecinos(punto, dataset, distancia, x, y);
+    let primera = primera_derivada(punto, neighbours, x, y);
     for(let i = 0; i < neighbours.length; i++)
     {
-        let punto_der = primera_derivada(neighbours[i], vecinos(neighbours[i], dataset, distancia);
-        derivada += (punto_der - primera)*(neighbours[i].x - punto.x);
-        division += (neighbours[i].x - punto.x)**2;
+        let punto_der = primera_derivada(neighbours[i], vecinos(neighbours[i], dataset, distancia, x, y), x, y);
+        derivada += (punto_der - primera)*(neighbours[i][x] - punto[x]);
+        division += (neighbours[i][x] - punto[x])**2;
     }
     return derivada/division;
 }
 
-function smoothness(dataset, distancia)
+function smoothness(dataset, distancia, x, y)
 {
     let c=0
     for(let i=0; i<dataset.length; i++)
     {
-        c += segunda_derivada(dataset[i], dataset, distancia);
+        c += segunda_derivada(dataset[i], dataset, distancia, x, y);
     }
     return 1/c;
 }
