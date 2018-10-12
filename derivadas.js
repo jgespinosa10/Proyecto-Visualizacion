49ac6401b5613c834df90834e706f23a5968a1f8
jgespinosa10 <jgespinosa@uc.cl>
2018-10-12 15:39:55 -0300

    arreglo de problema

--- derivadas.js
+++ derivadas.js
@@ -4,7 +4,7 @@ function vecinos(punto, dataset, distancia, x, y)
     for(let i = 0; i < dataset.length; i++)
     {
         let r = ((dataset[i][y] - punto[y])**2+(dataset[i][x] - punto[x])**2)**(1);
-        if(r<=distancia) neighbours.push(dataset[i]);
+        if(r != 0 && r<=distancia) neighbours.push(dataset[i]);
     }
     return neighbours;
 }
@@ -12,6 +12,7 @@ function vecinos(punto, dataset, distancia, x, y)
 // recibe los vecinos y calcula la derivada
 function primera_derivada(punto, neighbours, x, y)
 {
+    if(neighbours.length == 0) return 0;
     let derivada = 0;
     let division = 0;
     for(let i = 0; i < neighbours.length; i++)
@@ -20,6 +21,7 @@ function primera_derivada(punto, neighbours, x, y)
         derivada += (neighbours[i][y] - punto[y])*(deltax);
         division += (deltax)**2;
     }
+    if(division == 0) return 0; // Hay que preocuparse de los casos en que es infinito
     return derivada/division;
 }
 
@@ -35,6 +37,7 @@ function segunda_derivada(punto, dataset, distancia, x, y)
         derivada += (punto_der - primera)*(neighbours[i][x] - punto[x]);
         division += (neighbours[i][x] - punto[x])**2;
     }
+    if (division == 0) return 0;
     return derivada/division;
 }
 
